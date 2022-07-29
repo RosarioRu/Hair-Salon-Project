f6159517ec95534dd41c574b71974c1258e20c54
RosarioRu <mariarruval@gmail.com>
2022-07-28 20:42:59 -0700

    Add HttpGet route to display the Index view for Stylists.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -5,7 +5,12 @@ namespace HairSalon.Controllers
 {
   public class StylistsController : Controller
   {
-    
 
-  }
+    [HttpGet]
+    public ActionResult Index() 
+    { 
+      return View();
+    }
+
+  }  
 }
