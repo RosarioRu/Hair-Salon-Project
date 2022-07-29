3d8f053404977fc534e8741bc8b7eb06ff69ad3f
RosarioRu <mariarruval@gmail.com>
2022-07-28 20:22:00 -0700

    Add public virtual 'collection' of clients as a property for stylists, this 'Clients' will be all the clients under that stylist.

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -9,11 +9,12 @@ namespace HairSalon.Models
     public string StylistFirstName {get; set }
     public string StylistLastName {get; set }
     public string StylistSpecialty {get; set }
+    public virtual ICollection<Client> Clients { get; set; }
 
     public Stylist()
     {
       this.Clients = new HashSet<Client>();
-    }
+    } //will return all clients that belong to a stylist
 
   }
 }
